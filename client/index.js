204e94a1dc01fdaa43df02fd1056e30879cb4a3a
Akshat-Somvanshi <akshatsomvanshi22@gmail.com>
2023-06-07 19:06:26 +0530

    modified

--- client/index.js
+++ client/index.js
@@ -6,9 +6,27 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  if (process.argv.length < 3) {
+    console.error('Usage: node index.js [name]');
+    process.exit(1);
+  }
+  const name = process.argv[2].trim(); // see niceList.json for a full name list
+  
+  // prove to the server we're on the nice list
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot();
+  console.log(root);
+
+// find the proof that norman block is in the list 
+  const index = niceList.findIndex(n => n === name);
+
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name,
+    root,
+    proof,
   });
 
   console.log({ gift });
--- utils/example.js
+++ utils/example.js
@@ -1,19 +1,34 @@
+
 const MerkleTree = require('./MerkleTree');
 const niceList = require('./niceList');
 const verifyProof = require('./verifyProof');
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter your name to check if you are on the good list?: ', (name) => {
+  console.log('You entered: ' + name);
+
+  
 // create the merkle tree for the whole nice list
 const merkleTree = new MerkleTree(niceList);
 
 // get the root
 const root = merkleTree.getRoot();
+console.log(root);
 
 // find the proof that norman block is in the list 
-const name = 'Norman Block';
 const index = niceList.findIndex(n => n === name);
+
 const proof = merkleTree.getProof(index);
 
 // verify proof against the Merkle Root
 console.log( verifyProof(proof, name, root) ); // true, Norman Block is in the list!
 
 // TRY IT OUT: what happens if you try a name not in the list, or a fake proof?
+  rl.close();
+});
